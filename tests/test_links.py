999a01a9f32d58f55ebd705a2f7571d2f364f19d
Piyush Pal <xrpiyushh@gmail.com>
2026-07-23 22:43:22 +0530

    updated notes

--- tests/test_links.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from vault_utils import extract_wikilinks
-
-def test_extract_wikilinks():
-    text = "Here is a [[link]] and [[another-link|label]] and [[link-with#heading]]."
-    links = extract_wikilinks(text)
-    assert "link" in links
-    assert "another-link" in links
-    assert "link-with" in links
